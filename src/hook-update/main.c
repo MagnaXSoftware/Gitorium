728c0433e682d8127955f9af401a7b177c10b6d1
Xavier L. <xavier.l@afrosoft.tk>
2012-12-26 13:05:00 -0500

    Updating cmake files

--- src/hook-update/main.c
+++ src/hook-update/main.c
@@ -3,10 +3,28 @@
 int main(int argc, char **argv)
 {
     /* This program will have to:
-        * read main configuration
-        * read repo configuration from admin repo
-        * determine if user can push to this repo
-        * run each hook for the current repo
+     *  * read main configuration
+     *  * read repo configuration from admin repo
+     *  * determine if user can push to this repo
+     *  * run each hook for the current repo
      */
-    return EXIT_FAILURE;
+
+    // We remove the name of the executable from the list
+    argv++;
+    argc--;
+
+    if (gitorium_config_init())
+    {
+        PRINT_ERROR("Could not initialize the configuration.")
+        return EXIT_FAILURE;
+    }
+
+    // We receive arguments from git as such: name old_oid new_oid
+    if (argc != 3)
+    {
+        PRINT_ERROR("We didn't start as an update hook!")
+        return EXIT_FAILURE;
+    }
+
+    return 0;
 }

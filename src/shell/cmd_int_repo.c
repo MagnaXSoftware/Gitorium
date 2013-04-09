e5e8cebc37455b613ee8f55b0425a6f38d0e2994
Xavier L <xavier.l@afrosoft.tk>
2013-04-09 11:24:43 -0400

    Removing fsck from list of functions

--- src/shell/cmd_int_repo.c
+++ src/shell/cmd_int_repo.c
@@ -1,15 +1,1 @@
 #include "cmd_int_repo.h"
-
-int cmd_int_fsck(char *user, char **args)
-{
-	error("THIS COMMAND HAS NOT YET BEEN IMPLEMENTED.");
-	return -1;
-}
-
-int cmd_int_fsck_help(char *user, char **args)
-{
-	puts("fsck [--all|<repo> [...]]\n"
-		"\n"
-		"Runs a git fsck on the specified repositories or all.");
-	return 0;
-}

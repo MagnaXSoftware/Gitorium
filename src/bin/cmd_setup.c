1518d9c5e4bd498245571830bcb1e10ffeac2b9e
Xavier L <xavier.l@afrosoft.ca>
2013-04-22 22:33:42 -0400

    refactoring file opening

--- src/bin/cmd_setup.c
+++ src/bin/cmd_setup.c
@@ -19,58 +19,58 @@ static void setup__generate_conf(char **fullconf, char *user)
 		"{\n"
 		"    *admins = [\"",
 
-		"\"];\n"
-		"};\n"
-		"\n"
-		"/* Repositories \n"
-		" * \n"
-		" * Repositories are defined inside a {} pair.\n"
-		" * Each repository must have at least a name key. If there is no name key,\n"
-		" * that repository entry is considered non-valid.\n"
-		" * Repositories do not need to contain a perms key, but if they don't, then\n"
-		" * no one will have access to that repository.\n"
-		" * \n"
-		" *     repositories:\n"
-		" *     (\n"
-		" *         {\n"
-		" *             name = \"repo1\";\n"
-		" *             perms = {\n"
-		" *                 user1   = \"RW\"\n"
-		" *                 *group1 = \"R\"\n"
-		" *             }\n"
-		" *         },\n"
-		" *         {\n"
-		" *             name = \"repo2\";\n"
-		" *             perms = {\n"
-		" *                 user1   = \"R\"\n"
-		" *                 *group2 = \"RW\"\n"
-		" *             }\n"
-		" *         }\n"
-		" *     )\n"
+			"\"];\n"
+"};\n"
+"\n"
+"/* Repositories \n"
+" * \n"
+" * Repositories are defined inside a {} pair.\n"
+" * Each repository must have at least a name key. If there is no name key,\n"
+" * that repository entry is considered non-valid.\n"
+" * Repositories do not need to contain a perms key, but if they don't, then\n"
+" * no one will have access to that repository.\n"
+" * \n"
+" *     repositories:\n"
+" *     (\n"
+	" *         {\n"
+	" *             name = \"repo1\";\n"
+	" *             perms = {\n"
+	" *                 user1   = \"RW\"\n"
+	" *                 *group1 = \"R\"\n"
+	" *             }\n"
+	" *         },\n"
+	" *         {\n"
+	" *             name = \"repo2\";\n"
+	" *             perms = {\n"
+	" *                 user1   = \"R\"\n"
+	" *                 *group2 = \"RW\"\n"
+	" *             }\n"
+	" *         }\n"
+	" *     )\n"
 		" */\n"
-		"repositories:\n"
-		"(\n"
-		"    {\n"
-		"        name = \"gitorium-admin\";\n"
-		"        perms = {\n"
-		"            ",
+"repositories:\n"
+"(\n"
+	"    {\n"
+	"        name = \"gitorium-admin\";\n"
+	"        perms = {\n"
+	"            ",
 
-		" = \"RW\"\n"
-		"            *admins = \"RW\"\n"
-		"        }\n"
-		"    }\n"
-		");\n"
-	};
+	" = \"RW\"\n"
+	"            *admins = \"RW\"\n"
+	"        }\n"
+	"    }\n"
+	");\n"
+};
 
-	*fullconf = malloc(sizeof(char)*(strlen(parts[0])+1));
-	strcpy(*fullconf, parts[0]);
+*fullconf = malloc(sizeof(char)*(strlen(parts[0])+1));
+strcpy(*fullconf, parts[0]);
 
-	for(unsigned int i = 1; i < ARRAY_SIZE(parts); i++)
-	{
-		*fullconf = realloc(*fullconf, sizeof(char)*(strlen(*fullconf)+strlen(user)+strlen(parts[i])+1));
-		strcat(*fullconf, user);
-		strcat(*fullconf, parts[i]);
-	}
+for(unsigned int i = 1; i < ARRAY_SIZE(parts); i++)
+{
+	*fullconf = realloc(*fullconf, sizeof(char)*(strlen(*fullconf)+strlen(user)+strlen(parts[i])+1));
+	strcat(*fullconf, user);
+	strcat(*fullconf, parts[i]);
+}
 }
 
 static void exec_setup_push(void *payload)
@@ -121,275 +121,286 @@ static int setup__admin_repo(char *pubkey, int force)
 	setup__generate_conf(&conf, user);
 	free(user);
 
-	if ((pFile = fopen(pubkey, "r")))
-	{
-		if (!stat(".gitorium-admin", &rStat))
-			remove(".gitorium-admin");
-
-		if (git_repository_init(&repo, ".gitorium-admin", 0))
-		{
-			error("Could initialize a new administration repository.");
-			fclose(pFile);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_treebuilder_create(&rcBuilder, NULL))
-		{
-			error("Could not create keys tree builder.");
-			git_treebuilder_free(rcBuilder);
-			git_repository_free(repo);
-			fclose(pFile);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-// Copying the key
-		fseek(pFile , 0 , SEEK_END);
-		unsigned int size = ftell(pFile);
-		rewind(pFile);
-
-		buffer = malloc(sizeof(char) * size);
-		if (buffer == NULL)
-		{
-			error("Could not copy the public key.");
-			free(buffer);
-			git_treebuilder_free(rcBuilder);
-			git_repository_free(repo);
-			fclose(pFile);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		if (fread(buffer, sizeof(char), size, pFile) != size)
-		{
-			error("Could not copy the public key.");
-			free(buffer);
-			git_treebuilder_free(rcBuilder);
-			git_repository_free(repo);
-			fclose(pFile);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		fclose(pFile);
-
-		if (git_blob_create_frombuffer(&iOid, repo, buffer, sizeof(char)*size))
-		{
-			error("Could not create the administrator's public key.");
-			free(buffer);
-			git_treebuilder_free(rcBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		free(buffer);
-
-		if (git_treebuilder_insert(NULL, rcBuilder, pubkey, &iOid, 0100644))
-		{
-			error("Could not insert the administrator's public key.");
-			git_treebuilder_free(rcBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_treebuilder_write(&iOid, repo, rcBuilder))
-		{
-			error("Could not write the tree to index.");
-			git_treebuilder_free(rcBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		git_treebuilder_free(rcBuilder);
-
-		if (git_treebuilder_create(&rBuilder, NULL))
-		{
-			error("Could not create a tree builder.");
-			git_treebuilder_free(rBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_treebuilder_insert(NULL, rBuilder, "keys", &iOid, 040000))
-		{
-			error("Could not insert the keys tree.");
-			git_treebuilder_free(rBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_blob_create_frombuffer(&iOid, repo, conf, sizeof(char)*strlen(conf)))
-		{
-			error("Could not create the administration file.");
-			git_treebuilder_free(rBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_treebuilder_insert(NULL, rBuilder, "gitorium.conf", &iOid, 0100644))
-		{
-			error("Could not insert the administration file.");
-			git_treebuilder_free(rBuilder);
-			git_repository_free(repo);
-			free(conf);
-			return GITORIUM_ERROR;
-		}
-
-		free(conf);
-
-		if (git_treebuilder_write(&iOid, repo, rBuilder))
-		{
-			error("Could not write the tree to index.");
-			git_treebuilder_free(rBuilder);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		git_treebuilder_free(rBuilder);
-
-		if (git_tree_lookup(&rTree, repo, &iOid))
-		{
-			error("Could not write the tree to index.");
-			git_tree_free(rTree);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_signature_now(&rAuthor, "Gitorium", "gitorium@local"))
-		{
-			error("Could not create a commit author.");
-			git_tree_free(rTree);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		if (git_commit_create(&iOid, repo, "HEAD", rAuthor, rAuthor, NULL, "Initial Configuration", rTree, 0, NULL))
-		{
-			error("Could not create the commit.");
-			git_signature_free(rAuthor);
-			git_tree_free(rTree);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		git_signature_free(rAuthor);
-		git_tree_free(rTree);
-
-//push the commit to origin
-
-		if (!stat(rFullpath, &rStat))
-		{
-			if (rrmdir(rFullpath))
-			{
-				error("Failed to launch external program 'rm'.");
-				error("Please remove the admin directory manually.");
-				errorf("%s\n", rFullpath);
-				free(rFullpath);
-				git_repository_free(repo);
-				return GITORIUM_EXTERN;
-			}
-		}
-
-		if (git_repository_init(&bRepo, rFullpath, 1))
-		{
-			error("Could not initialize the remote admin repository.");
-			free(rFullpath);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		git_repository_free(bRepo);
-
-		char *puFullpath = malloc(sizeof(char) * (strlen(rFullpath) + strlen("/hooks/post-update") + 1));
-		if (NULL == puFullpath)
-		{
-			free(rFullpath);
-			git_repository_free(repo);
-			return GITORIUM_MEM_ALLOC;
-		}
-		strcat(strcpy(puFullpath, rFullpath), "/hooks/post-update");
-		symlink(CMAKE_INSTALL_PREFIX"/bin/gitorium-hook-admin", puFullpath);
-		free(puFullpath);
-
-		char *uFullpath = malloc(sizeof(char) * (strlen(rFullpath) + strlen("/hooks/update") + 1));
-		if (NULL == uFullpath)
-		{
-			free(rFullpath);
-			git_repository_free(repo);
-			return GITORIUM_MEM_ALLOC;
-		}
-		strcat(strcpy(uFullpath, rFullpath), "/hooks/update");
-		symlink(CMAKE_INSTALL_PREFIX"/bin/gitorium-hook-admin", uFullpath);
-		free(uFullpath);
-
-		rUrl = malloc(sizeof("file://") + sizeof(char)*(strlen(rFullpath) + 1));
-		if (NULL == rUrl)
-		{
-			free(rFullpath);
-			git_repository_free(repo);
-			return GITORIUM_MEM_ALLOC;
-		}
-		strcat(strcpy(rUrl, "file://"), rFullpath);
-		free(rFullpath);
-
-		if (git_remote_create(&rRemote, repo, "origin", rUrl))
-		{
-			error("Could not add the bare repository as remote.");
-			git_remote_free(rRemote);
-			free(rUrl);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		free(rUrl);
-
-		#ifndef _NO_GIT2_PUSH  // libgit2 cannot push repositories ATM, so we must call git.
-		if (git_remote_connect(rRemote, GIT_DIRECTION_PUSH))
-		{
-			error("Could not push the repository.");
-			git_remote_disconnect(rRemote);
-			git_remote_free(rRemote);
-			git_repository_free(repo);
-			return GITORIUM_ERROR;
-		}
-
-		git_remote_disconnect(rRemote);
-		#else
-		if (gitorium_execlp(&exec_setup_push, (void *) ".gitorium-admin", "git", "push", "origin", "master", (char *) NULL))
-		{
-			error("Failed to launch external program 'git push'.");
-			git_remote_free(rRemote);
-			git_repository_free(repo);
-			return GITORIUM_EXTERN;
-		}
-		#endif
-
-		git_remote_free(rRemote);
-
-		if (rrmdir(".gitorium-admin"))
-		{
-			error("Failed to launch external program 'rm'.");
-			error("Please remove the .gitorium-admin directory manually.");
-			git_repository_free(repo);
-			return GITORIUM_EXTERN;
-		}
-
-		git_repository_free(repo);
-		return 0;
-	}
-	else
+	if (NULL == (pFile = fopen(pubkey, "r")))
 	{
 		errorf("The public key file '%s' doesn't exist.", pubkey);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+	
+	if (!stat(".gitorium-admin", &rStat))
+		remove(".gitorium-admin");
+
+	if (git_repository_init(&repo, ".gitorium-admin", 0))
+	{
+		error("Could initialize a new administration repository.");
+		fclose(pFile);
+		free(conf);
+		free(rFullpath);
 		return GITORIUM_ERROR;
 	}
 
+	if (git_treebuilder_create(&rcBuilder, NULL))
+	{
+		error("Could not create keys tree builder.");
+		git_treebuilder_free(rcBuilder);
+		git_repository_free(repo);
+		fclose(pFile);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+// Copying the key
+	fseek(pFile , 0 , SEEK_END);
+	unsigned int size = ftell(pFile);
+	rewind(pFile);
+
+	buffer = malloc(sizeof(char) * size);
+	if (buffer == NULL)
+	{
+		error("Could not copy the public key.");
+		git_treebuilder_free(rcBuilder);
+		git_repository_free(repo);
+		fclose(pFile);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (fread(buffer, sizeof(char), size, pFile) != size)
+	{
+		error("Could not copy the public key.");
+		free(buffer);
+		git_treebuilder_free(rcBuilder);
+		git_repository_free(repo);
+		fclose(pFile);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	fclose(pFile);
+
+	if (git_blob_create_frombuffer(&iOid, repo, buffer, sizeof(char)*size))
+	{
+		error("Could not create the administrator's public key.");
+		free(buffer);
+		git_treebuilder_free(rcBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	free(buffer);
+
+	if (git_treebuilder_insert(NULL, rcBuilder, pubkey, &iOid, 0100644))
+	{
+		error("Could not insert the administrator's public key.");
+		git_treebuilder_free(rcBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (git_treebuilder_write(&iOid, repo, rcBuilder))
+	{
+		error("Could not write the tree to index.");
+		git_treebuilder_free(rcBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	git_treebuilder_free(rcBuilder);
+
+	if (git_treebuilder_create(&rBuilder, NULL))
+	{
+		error("Could not create a tree builder.");
+		git_treebuilder_free(rBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (git_treebuilder_insert(NULL, rBuilder, "keys", &iOid, 040000))
+	{
+		error("Could not insert the keys tree.");
+		git_treebuilder_free(rBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (git_blob_create_frombuffer(&iOid, repo, conf, sizeof(char)*strlen(conf)))
+	{
+		error("Could not create the administration file.");
+		git_treebuilder_free(rBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (git_treebuilder_insert(NULL, rBuilder, "gitorium.conf", &iOid, 0100644))
+	{
+		error("Could not insert the administration file.");
+		git_treebuilder_free(rBuilder);
+		git_repository_free(repo);
+		free(conf);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	free(conf);
+
+	if (git_treebuilder_write(&iOid, repo, rBuilder))
+	{
+		error("Could not write the tree to index.");
+		git_treebuilder_free(rBuilder);
+		git_repository_free(repo);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	git_treebuilder_free(rBuilder);
+
+	if (git_tree_lookup(&rTree, repo, &iOid))
+	{
+		error("Could not write the tree to index.");
+		git_tree_free(rTree);
+		git_repository_free(repo);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (git_signature_now(&rAuthor, "Gitorium", "gitorium@local"))
+	{
+		error("Could not create a commit author.");
+		git_tree_free(rTree);
+		git_repository_free(repo);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	if (git_commit_create(&iOid, repo, "HEAD", rAuthor, rAuthor, NULL, "Initial Configuration", rTree, 0, NULL))
+	{
+		error("Could not create the commit.");
+		git_signature_free(rAuthor);
+		git_tree_free(rTree);
+		git_repository_free(repo);
+		free(rFullpath);
+		return GITORIUM_ERROR;
+	}
+
+	git_signature_free(rAuthor);
+	git_tree_free(rTree);
+
+//push the commit to origin
+
+	if (!stat(rFullpath, &rStat))
+	{
+		if (rrmdir(rFullpath))
+		{
+			error("Failed to launch external program 'rm'.");
+			error("Please remove the admin directory manually.");
+			errorf("%s\n", rFullpath);
+			free(rFullpath);
+			git_repository_free(repo);
+			return GITORIUM_EXTERN;
+		}
+	}
+
+	if (git_repository_init(&bRepo, rFullpath, 1))
+	{
+		error("Could not initialize the remote admin repository.");
+		free(rFullpath);
+		git_repository_free(repo);
+		return GITORIUM_ERROR;
+	}
+
+	git_repository_free(bRepo);
+
+	char *puFullpath = malloc(sizeof(char) * (strlen(rFullpath) + strlen("/hooks/post-update") + 1));
+	if (NULL == puFullpath)
+	{
+		free(rFullpath);
+		git_repository_free(repo);
+		return GITORIUM_MEM_ALLOC;
+	}
+	strcat(strcpy(puFullpath, rFullpath), "/hooks/post-update");
+	symlink(CMAKE_INSTALL_PREFIX"/bin/gitorium-hook-admin", puFullpath);
+	free(puFullpath);
+
+	char *uFullpath = malloc(sizeof(char) * (strlen(rFullpath) + strlen("/hooks/update") + 1));
+	if (NULL == uFullpath)
+	{
+		free(rFullpath);
+		git_repository_free(repo);
+		return GITORIUM_MEM_ALLOC;
+	}
+	strcat(strcpy(uFullpath, rFullpath), "/hooks/update");
+	symlink(CMAKE_INSTALL_PREFIX"/bin/gitorium-hook-admin", uFullpath);
+	free(uFullpath);
+
+	rUrl = malloc(sizeof("file://") + sizeof(char)*(strlen(rFullpath) + 1));
+	if (NULL == rUrl)
+	{
+		free(rFullpath);
+		git_repository_free(repo);
+		return GITORIUM_MEM_ALLOC;
+	}
+	strcat(strcpy(rUrl, "file://"), rFullpath);
+	free(rFullpath);
+
+	if (git_remote_create(&rRemote, repo, "origin", rUrl))
+	{
+		error("Could not add the bare repository as remote.");
+		git_remote_free(rRemote);
+		free(rUrl);
+		git_repository_free(repo);
+		return GITORIUM_ERROR;
+	}
+
+	free(rUrl);
+
+		#ifndef _NO_GIT2_PUSH  // libgit2 cannot push repositories ATM, so we must call git.
+	if (git_remote_connect(rRemote, GIT_DIRECTION_PUSH))
+	{
+		error("Could not push the repository.");
+		git_remote_disconnect(rRemote);
+		git_remote_free(rRemote);
+		git_repository_free(repo);
+		return GITORIUM_ERROR;
+	}
+
+	git_remote_disconnect(rRemote);
+		#else
+	if (gitorium_execlp(&exec_setup_push, (void *) ".gitorium-admin", "git", "push", "origin", "master", (char *) NULL))
+	{
+		error("Failed to launch external program 'git push'.");
+		git_remote_free(rRemote);
+		git_repository_free(repo);
+		return GITORIUM_EXTERN;
+	}
+		#endif
+
+	git_remote_free(rRemote);
+
+	if (rrmdir(".gitorium-admin"))
+	{
+		error("Failed to launch external program 'rm'.");
+		error("Please remove the .gitorium-admin directory manually.");
+		git_repository_free(repo);
+		return GITORIUM_EXTERN;
+	}
+
+	git_repository_free(repo);
 	return 0;
 }
 
